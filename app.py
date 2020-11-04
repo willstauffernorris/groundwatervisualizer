40f5b92dddc42ebc6c1cd023a78509b330aa715e
willsn <willsn@gmail.com>
2020-11-04 15:54:25 -0700

    add server line in app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
-
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -14,6 +14,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 # df = pd.DataFrame({
